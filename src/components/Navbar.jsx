85518a4a86a4c94a115cf0c66f6aa4246de3d423
Yadav Amarjit <yadavamarjit772@gmail.com>
2023-09-11 10:48:09 +0530

    changed title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,10 +45,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Yadav Amerjit &nbsp;
-            <span className="sm:block hidden">
-              {" "}
-              | Junior Software Developer
-            </span>
+            <span className="sm:block hidden"> | Software Developer</span>
           </p>
         </Link>
 
