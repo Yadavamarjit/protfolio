c2c6e3d6b0526c79c0cfb72f4762c9fe9bae1c97
Yadav Amarjit <yadavamarjit772@gmmail.com>
2023-12-12 00:32:31 +0530

    changed name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Yadav Amerjit</span>
+            Hi, I'm <span className="text-[#915EFF]">Yadav Amarjit</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Fullstack Dev, crafting seamless experiences.
